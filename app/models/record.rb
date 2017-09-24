7020026ff68b09a862fada41ea70b639edf5d7fc
Bipin Manandhar <kubipin07@gmail.com>
2017-09-24 12:22:36 +0545

    add background image and logic to record model

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,16 @@
 class Record < ActiveRecord::Base
   validates :date, :title, :amount, presence: true
 end
+
+public
+def get_balance
+  Record.where("amount > 0").sum(:amount)
+end
+
+def get_debt
+  Record.where("amount < 0").sum(:amount)
+end
+
+def get_total
+  Record.sum(:amount)
+end
